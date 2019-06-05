62a848e37db695da996fe991eac66c095e7daf76
Lubos Lacina <lacom@email.cz>
2019-06-05 13:51:29 +0200

    Init Trader process

--- src/main/kotlin/com/wbtcb/process/trade/RabbitMqConstants.kt
+++ src/main/kotlin/com/wbtcb/process/trade/RabbitMqConstants.kt
@@ -3,10 +3,13 @@ package com.wbtcb.process.trade
 class RabbitMqConstants {
 
     companion object {
-        // Trade exchange
+        /*
+            Trade exchange
+         */
         const val TRADE_EXCHANGE = "trade"
-
-        // Routing key
-        const val TRADE_ROUTING_PREFIX_KEY = TRADE_EXCHANGE
-   }
+        /*
+            Routing key
+         */
+        const val TRADE_ROUTING_PREFIX_KEY = RabbitMqConstants.TRADE_EXCHANGE
+    }
 }
