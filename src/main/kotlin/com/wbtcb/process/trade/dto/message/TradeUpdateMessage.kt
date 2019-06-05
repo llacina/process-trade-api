ca5b884104877920c40ead2a160d176d1bd6424d
Lubos Lacina <lacom@email.cz>
2019-06-05 13:00:12 +0200

    Init Trader process

--- src/main/kotlin/com/wbtcb/process/trade/dto/message/TradeUpdateMessage.kt
+++ src/main/kotlin/com/wbtcb/process/trade/dto/message/TradeUpdateMessage.kt
@@ -1,13 +1,17 @@
 package com.wbtcb.process.trade.message
 
+import com.wbtcb.process.trade.enum.TradeStatus
 import java.io.Serializable
 import java.math.BigDecimal
 import java.util.UUID
 
 data class TradeUpdateMessage(
+    val id: Int,
     val orderRef: UUID,
-    val channelCode: String,
-    val stockCode: String,
-    val amount: BigDecimal
-    // TODO
+    val exchangeRequestId: Int,
+    val amount: BigDecimal,
+    val status: TradeStatus,
+    val reasonCode: String? = null,
+    val reasonTitle: String? = null,
+    val reasonDetail: String? = null
 ) : Serializable
